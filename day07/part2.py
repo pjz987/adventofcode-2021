7a6b28bfd94c34de5f13bc7b0753903987b459d8
pjz987 <pwj2012@gmail.com>
2021-12-07 07:41:58 -0800

    day07

---
+++ day07/part2.py
@@ -0,0 +1,21 @@
+filepath = 'day07/input.txt'
+with open(filepath, 'r') as f:
+    crabs = list(map(lambda crab: int(crab), f.read().split(',')))
+
+# crabs = list(map(lambda crab: int(crab), '16,1,2,0,4,2,7,1,2,14'.split(',')))
+
+print(crabs)
+print(len(crabs), min(crabs), max(crabs))
+print(sum(crabs) / len(crabs))
+
+total_movements = []
+
+for pos in range(min(crabs), max(crabs)):
+    print(pos)
+    total_movement = 0
+    for crab in crabs:
+        total_movement += sum(range(abs(crab - pos) + 1))
+    total_movements.append(total_movement)
+
+print(min(total_movements))
+
